9a0656f982bca9a896338a510488e10b71364b5c
Shuang Ma <mashuangpsy@gmail.com>
2016-08-16 17:09:50 +0800

    exercise 3

---
+++ ex3.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# exercise 3  
+
+print "I will now count my chickens:"  # print text
+
+print "Hens", 25 + 30 / 6  # print text & result, separated by a comma
+print "Roosters", 100 - 25 * 3 % 4  
+
+print "Now I will count the eggs:"  
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # print result
+
+print "Is it true that 3 + 2 < 5 - 7?"  
+
+print 3 + 2 < 5 - 7  # print the comparison result (True or False)
+
+print "What is 3 + 2?", 3 + 2  
+print "What is 5 - 7?", 5 - 7  
+
+print "Oh, that's why it's False."  
+
+print "How about some more."  
+
+print "Is it greater?", 5 > -2  
+print "Is it greater or equal?", 5 >= -2  
+print "Is it less or equal?", 5 <= -2  
+
+# notes
+
+# `%` 用于计算余数，如 4 % 2 ，结果为 0
+
+# 当数字为整数，`/` 求出的为整数商，如 7 / 4 ，结果为 1   
+# 若要得到更精确结果，使用浮点数（floating point number）, 如 7.0 / 4，商为 1.75
+
+# 运算顺序  
+# PEMDAS：Parentheses | Exponents | Multiplication Division | Addition Subtraction
