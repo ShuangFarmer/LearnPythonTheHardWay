4ca73df2690db06042eb5b39f5c220f6b4213b7f
Shuang Ma <mashuangpsy@gmail.com>
2016-08-16 17:09:36 +0800

    exercise 2

---
+++ ex2.py
@@ -0,0 +1,15 @@
+# exercise 2  
+
+# A comment, this is so you can read your program later.  
+# Anything after the # is ignored by python.  
+
+print "I could have code like this." # and the comment after is ignored  
+
+# you can also use a comment to "disable" or comment out a piece of code:  
+# print "This won't run."  
+
+print "This will run."   
+
+# A good way of checking your codes  
+# review each line going backward.  
+# in this way, your brain is kind of fed on new pieces of information and can easily spot bugs.
