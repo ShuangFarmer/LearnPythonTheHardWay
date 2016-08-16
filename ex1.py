9820deb524700e7da67875f667afdbf738500780
Shuang Ma <mashuangpsy@gmail.com>
2016-08-16 17:09:16 +0800

    exercise 1

---
+++ ex1.py
@@ -0,0 +1,11 @@
+# exercise 1
+# if getting errors about ASCII encodings
+# put `# -*- coding: utf-8 -*-` at the top of the Python script
+
+print "Hello World!"  
+print "Hello Again"  
+print "I like typing this."  
+print "This is fun."  
+print 'Yay! Printing.'  
+print "I'd much rather you 'not'."  
+print 'I "said" do not touch this.' 
