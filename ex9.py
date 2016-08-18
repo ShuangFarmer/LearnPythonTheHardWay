d5b2685e6a6d97571a1c1265b2d028d758fa5b19
Shuang Ma <mashuangpsy@gmail.com>
2016-08-18 16:06:24 +0800

    add exercise 9

---
+++ ex9.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# exercise 9  
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" # `\n` 用于另起一行
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+# print 出的格式将与 `“”“` 框住的文字一致
+# ”“” 与 ”“”， " 与 " ，' 与 ‘，需要配对使用
+print """
+There's something going on here. 
+With the three double-quotes. 
+We'll be able to type as much as we like. 
+Even 4 lines if we want, or 5, or 6.
+"""
