ced518d2f3f7afe41ef7f5a01b587a0e8cef6795
Shuang Ma <mashuangpsy@gmail.com>
2016-08-19 11:34:05 +0800

    add exercise 13

---
+++ ex13.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# exercise 13  
+
+from sys import argv # sys: library argv: module
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+# when runing the code in Terminal, 
+# remember to input arguments  
+# $ python ex13.py first 2nd 3rd
