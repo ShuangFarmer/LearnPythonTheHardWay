aa08f972bb74f4a2e6979ca496bfc0747bced6b0
Shuang Ma <mashuangpsy@gmail.com>
2016-08-21 21:43:30 +0800

    add exercise 15 study drill use raw_input instead of arguments

--- ex15.py
+++ ex15.py
@@ -2,16 +2,17 @@
 # exercise 15  
 
 # from the library sys import module argv
-from sys import argv  
+#from sys import argv  
 
 # set two arguments for this script
-script, filename = argv  
+#script, filename = argv  
 
 # open(): Open a file, returning an object of the file type described in section File Objects
 # txt is a variable and the value of open(filename) will be assigned to the variable txt
+filename = raw_input("what is your file name?" )
 txt = open(filename)
 
 # print the txt
 print "Here's your file %r:" % filename
-# read() is a function that operate the variable txt 
+# read() is a function/method that operate the variable txt, AKA an object 
 print txt.read()
