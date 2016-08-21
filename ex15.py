7cfbfb4839c377def613d4accf259fcd4c2cd468
Shuang Ma <mashuangpsy@gmail.com>
2016-08-21 21:38:36 +0800

    add exercise 15

---
+++ ex15.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# exercise 15  
+
+# from the library sys import module argv
+from sys import argv  
+
+# set two arguments for this script
+script, filename = argv  
+
+# open(): Open a file, returning an object of the file type described in section File Objects
+# txt is a variable and the value of open(filename) will be assigned to the variable txt
+txt = open(filename)
+
+# print the txt
+print "Here's your file %r:" % filename
+# read() is a function that operate the variable txt 
+print txt.read()
+
+print "Type the filename again:"
+file_again = raw_input("> ")
+
+txt_again = open(file_again)
+
+print txt_again.read()
