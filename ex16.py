8164540cda63a71d2b374abe12e21dd7b3f1e710
Shuang Ma <mashuangpsy@gmail.com>
2016-08-22 10:33:14 +0800

    add exercise 16 study drills

--- ex16.py
+++ ex16.py
@@ -25,7 +25,8 @@ line3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file."
 
-target.write("%s\n%s\n%s\n" % (line1, line2, line3))
+content = "%s\n%s\n%s\n" % (line1, line2, line3)
+target.write(content)
 
 print "And finally, we close it."
 target.close()
@@ -36,3 +37,8 @@ print see.read()
 
 print "Let's close it again"
 see.close()
+
+## 
+# open(filename, 'r') <- reading
+# open(filename, 'w') <- writing/truncate
+# open(filename, 'a') <- appending
